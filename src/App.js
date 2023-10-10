594ccafeb43b030789980a7dfb706498e318f238
navneet17360 <navneetanand.2021cs@technonjr.org>
2023-10-10 12:29:58 +0530

    Travel-List Project

---
+++ src/App.js
@@ -0,0 +1,30 @@
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+function Logo() {
+  return <h1>🌴 Far Away 👜</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What Do You need for your 😍 trip </h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have X item on your list , you already packed X (X%)</em>
+    </footer>
+  );
+}
